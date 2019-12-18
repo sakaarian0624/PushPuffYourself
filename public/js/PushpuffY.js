15b1c8f8715bfdeca062f37f7cda745d564d890a
user <>
2019-12-18 10:38:43 +0900

    slider

--- public/js/PushpuffY.js
+++ public/js/PushpuffY.js
@@ -1,6 +1,7 @@
 var x = 1;
 var xs = "";
 var onoff = 0;
+var sliderClear = 0;
 var pushobject = {
 	"0": "エラー",
 };
@@ -21,14 +22,14 @@ var pushMake = ()=>{
     */
     console.log(limit);
 }
-var pushSet = ()=>{ const timer =　setInterval(()=>{
+/*var pushSet = ()=>{ const timer =　setInterval(()=>{
 	if (onoff == 0){
 		clearInterval(timer);
 	} else if(onoff == 1){
 	   pushMake();
 	}
 }
-,10000);};
+,10000);};*/
 var subNone = ()=>{
     var subjects = document.getElementsByClassName("subflex")
                 var index = 0, length = subjects.length;
@@ -41,9 +42,24 @@ new Vue ({
     data: {
         message: 'ファイルが選択されておりません',
         written: '通知内容を入力してください',
-        title: 'タイトルを入力してください'
+        title: 'タイトルを入力してください',
+        sliderNumber: 10
     },
     methods: {
+        pushSet:function(){
+            var timer =　setInterval(()=>{
+	           if (onoff == 0){
+		           clearInterval(timer);
+	           } else if(sliderClear == 1){
+                   sliderClear = 0;
+                   clearInterval(timer);
+                   this.pushSet();
+                   console.log("sliderClear");
+               } else if(onoff == 1){
+	               pushMake();
+	           }
+            }, this.sliderNumber * 1000);
+        },
         change:function(subject){
             const promise = new Promise((resolve)=>{
                 subNone();
@@ -172,20 +188,27 @@ new Vue ({
             Push.create("通知は正常に送られました");
         },
         pushStart:function(){
-        	if (onoff == 0){
-	        	Push.create("通知が開始されました");
-	        	onoff = 1;
-	        	pushSet();
-	        	console.log(onoff);
-	        	document.getElementById("onoff").classList.remove("parts3");
-	        	document.getElementById("onoff").classList.add("parts3on");
-        	}else if( onoff == 1){
+            if(onoff == 1){
         		Push.create("通知が終了されました");
+                console.log("pushEnd");
         		onoff = 0;
+                //clearInterval(this.timer);
         		console.log(onoff);
         		document.getElementById("onoff").classList.remove("parts3on");
 	        	document.getElementById("onoff").classList.add("parts3");
+        	} else if(onoff == 0){
+	        	Push.create("通知が開始されました");
+                console.log("pushStart");
+	        	onoff = 1;
+	        	this.pushSet();
+	        	console.log(onoff);
+	        	document.getElementById("onoff").classList.remove("parts3");
+	        	document.getElementById("onoff").classList.add("parts3on");
         	}
+        },
+        sliderChange:function(){
+            Push.create("通知間隔が変更されました");
+            sliderClear = 1;
         },
         pushSelect:function(eventname){
         	Push.create("科目が指定されました");
